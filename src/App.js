642c8032a815d9c0b6b9bc0fbc165010f111ceaa
josuaLagat <josua.lagat@volenday.com>
2022-02-03 21:49:35 +0800

    initial design/layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style.css";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='calculator-grid'>
+      <div className='output'>
+        <div className='previous-operand'>123,900 *</div>
+        <div className='current-operand'>123,123</div>
+      </div>
+
+      <button className='col-two btn-fn'>AC</button>
+      <button className='btn-fn'>DEL</button>
+      <button className='btn-operator'>÷</button>
+      <button className='btn-number'>1</button>
+      <button className='btn-number'>2</button>
+      <button className='btn-number'>3</button>
+      <button className='btn-operator'>*</button>
+      <button className='btn-number'>4</button>
+      <button className='btn-number'>5</button>
+      <button className='btn-number'>6</button>
+      <button className='btn-operator'>+</button>
+      <button className='btn-number'>7</button>
+      <button className='btn-number'>8</button>
+      <button className='btn-number'>9</button>
+      <button className='btn-operator'>-</button>
+      <button className='btn-fn'>%</button>
+      <button className='btn-number'>0</button>
+      <button className='btn-fn'>.</button>
+      <button className='btn-operator'>=</button>
     </div>
   );
 }
